276da01f202752595ea1b76bb19ab0228cbf3875
djantzlur <31110424+djantzlur@users.noreply.github.com>
2025-05-08 07:42:14 -0700

    Update my_custom_playbook.py

--- playbooks/my_custom_playbook.py
+++ playbooks/my_custom_playbook.py
@@ -5,5 +5,5 @@ def my_custom_action(event: ExecutionBaseEvent):
     print("Hello from my custom action!")
     prop=event.__dict__
     print(f"Custom Action - {prop}")
-    event.add_enrichment([JsonBlock({"message":"Hello Json Custom Action"})])
+    event.add_enrichment([JsonBlock('{"message":"Hello Json Custom Action"}')])
 
