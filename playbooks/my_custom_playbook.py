629e01da06ff9765dfe68fdb5f3e0d33ab742e80
djantzlur <31110424+djantzlur@users.noreply.github.com>
2025-05-08 07:30:38 -0700

    Update my_custom_playbook.py

--- playbooks/my_custom_playbook.py
+++ playbooks/my_custom_playbook.py
@@ -3,5 +3,7 @@ from robusta.api import *
 @action
 def my_custom_action(event: ExecutionBaseEvent):
     print("Hello from my custom action!")
+    prop=event.__dict__
+    print(f"Custom Action - {prop}")
     event.add_enrichment([JsonBlock("message":"Hello Json Custom Action")])
 
