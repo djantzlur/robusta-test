1b4fc03e0a9496abb1a5483f5b3a63fde22ac7ba
djantzlur <31110424+djantzlur@users.noreply.github.com>
2025-05-08 09:45:57 -0700

    Update my_custom_playbook.py

--- playbooks/my_custom_playbook.py
+++ playbooks/my_custom_playbook.py
@@ -1,19 +1,24 @@
 from robusta.api import *
+import json
 
 @action
 def my_custom_action(event: PodChangeEvent):
     print("Hello from my custom action!")
-    for o in event.__dict__:
-        print(o)
-        print('---')
-    print('add')
-    event.add_enrichment([JsonBlock('{"message":"Hello Json Custom Action"}')])
+    # for o in event.__dict__:
+    #     print(o)
+    #     print('---')
+    # print('add')
+    podName=event.obj.name
+    podNamespace=event.obj.namespace
+    message=json.dumps({'message':f'pod {podName} in {podNamespace} updated - {event.response}'})
+    event.add_enrichment([JsonBlock(message)])
     
     res = f'{event.filtered_diffs} - {event.description} - {event.operation}'
     print(f"Custom Action - {res}")
 
-    res = f'{event.response} - {event.obj}'
-    print(f"Custom Action - {res}")
+    for o in event.__dict__.obj:
+        print(o)
+        print('---')
     
     
 
